c387863b32364f50b241431ca6e72685f4f5fd77
Peter Boráros <pborky@pborky.sk>
2013-07-11 19:10:17 +0200

    remove boilerplate

--- project/views.py
+++ project/views.py
@@ -1,14 +1,14 @@
 __author__ = 'pborky'
 
 from django.contrib import messages
-from django.views.decorators import cache, http
-from django.contrib.auth.decorators import login_required
+from django.views.decorators import cache
+from django.shortcuts import redirect
 
-from .helpers import view
+from .helpers import view_POST, view_GET, combine
 from .forms import LoginForm
 
 
-@view(
+@view_POST(
     r'^login/do$',
     form_cls = {'login':LoginForm,},
     invalid_form_msg = 'Login form invalid.',
@@ -16,48 +16,45 @@ from .forms import LoginForm
     redirect_attr = 'next',
     decorators = ( cache.never_cache,  ),
 )
-class do_login:
-    @staticmethod
-    def post(request, forms):
-        from django.contrib.auth import login,authenticate
-        form = forms['login']
-        try:
-            if form.is_valid():
-                user = authenticate(username=form.data.get('username'), password=form.data.get('password'))
-                if user is not None:
-                    if user.is_active:
-                        login(request, user)
-                        messages.success(request, 'User authentication was successful.')
-                        return
-                    else:
-                        messages.error(request, 'User account has been disabled.')
-                        return
-        except Exception:
-            pass
-        messages.error(request, 'User authentication was unsuccessful.')
+def do_login(request, forms):
+    from django.contrib.auth import login,authenticate
+    form = forms['login']
+    try:
+        if form.is_valid():
+            user = authenticate(username=form.data.get('username'), password=form.data.get('password'))
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, 'User authentication was successful.')
+                    return
+                else:
+                    messages.error(request, 'User account has been disabled.')
+                    return
+    except Exception:
+        pass
+    messages.error(request, 'User authentication was unsuccessful.')
 
 
-@view(
+@view_GET(
     r'^login$',
     template = 'login.html',
     decorators = ( cache.never_cache,  ),
 )
-class login:
-    @staticmethod
-    def get(request, forms):
-        return {
-            'next': request.GET['next'] if 'next' in request.GET else request.POST['next'] if 'next' in request.POST else '/'
-        }
+def login(request, forms):
+    next = combine(request.GET, request.POST).get('next', '/')
+    if request.user and request.user.is_authenticated():
+        return redirect(next)
+    return {
+        'next': next
+    }
 
-@view(
+@view_GET(
     r'^logout$',
     redirect_to = '/',
     redirect_attr = 'next',
     decorators = ( cache.never_cache,  ),
 )
-class logout:
-    @staticmethod
-    def get(request, forms):
-        from django.contrib.auth import logout
-        logout(request)
-        messages.success(request, 'User deauthentication successful.')
+def logout(request, forms):
+    from django.contrib.auth import logout
+    logout(request)
+    messages.success(request, 'User deauthentication successful.')
